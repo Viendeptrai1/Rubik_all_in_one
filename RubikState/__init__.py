44d364c27879c023d3308237817781f2358a5caf
Kotori <phanquocvien.123@gmail.com>
2025-03-30 12:37:33 +0700

    fix done

---
+++ RubikState/__init__.py
@@ -0,0 +1,3 @@
+# Package for RubikState
+from RubikState.rubik_chen import RubikState, SOLVED_STATE as SOLVED_STATE_3X3
+from RubikState.rubik_2x2 import Rubik2x2State, SOLVED_STATE as SOLVED_STATE_2X2 
